d989a6c7e2f97713ae28c08d1a80792bd7025eb0
Haris Kamal Rana <harriskamal23@gmail.com>
2026-07-03 11:55:15 +0500

    Implemented fleetmanager

---
+++ backend/fleet/vehicle_registry.py
@@ -0,0 +1,27 @@
+VEHICLES = [
+
+    {
+        "vehicle_id": "CAR001",
+        "driver_id": "DRV001",
+        "vehicle_type": "city_car"
+    },
+
+    {
+        "vehicle_id": "CAR002",
+        "driver_id": "DRV002",
+        "vehicle_type": "aggressive_driver"
+    },
+
+    {
+        "vehicle_id": "CAR003",
+        "driver_id": "DRV003",
+        "vehicle_type": "highway_vehicle"
+    },
+
+    {
+        "vehicle_id": "VAN001",
+        "driver_id": "DRV004",
+        "vehicle_type": "delivery_van"
+    }
+
+]
